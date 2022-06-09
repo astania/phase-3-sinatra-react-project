b626ead506ebf24eb065776e5bee1974dd254ce5
+ <+>
2022-06-09 13:10:18 -0400

    update restaurants and guests controllers to include reservation data when returning JSON

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -2,18 +2,18 @@ class GuestsController < ApplicationController
     
     get "/guests" do 
         guests = Guest.all
-        guests.to_json
+        guests.to_json(include: [reservations: {include: { restaurant: { only: [:id, :restaurant_name, :cuisine_type, :image_url ]}}}])
     end 
 
     get "/guests/:id" do 
         guest = Guest.find(params[:id])
-        guest.to_json
+        guest.to_json(include: [reservations: {include: { restaurant: { only: [:id, :restaurant_name, :cuisine_type, :image_url ]}}}])
     end 
 
     post "/guests" do 
         guest = Guest.new(params[:guest])
         if guest.save
-            guest.to_json
+            guest.to_json(include: [reservations: {include: { restaurant: { only: [:id, :restaurant_name, :cuisine_type, :image_url ]}}}])
         else
             {errors: guest.errors.full_messages}.to_json  
         end 
@@ -28,6 +28,6 @@ class GuestsController < ApplicationController
     patch "/guests/:id" do 
         guest = Guest.find(params[:id])
         guest.update(params[:guest])
-        guest.to_json
+        guest.to_json(include: [reservations: {include: { restaurant: { only: [:id, :restaurant_name, :cuisine_type, :image_url ]}}}])
     end 
 end 
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,18 +2,19 @@ class RestaurantsController < ApplicationController
 
     get "/restaurants" do 
         restaurants = Restaurant.all
-        restaurants.to_json
+        restaurants.to_json(include: [reservations: {include: { guest: { only: [:name ]}}}])
     end 
 
     get "/restaurants/:id" do 
         restaurant = Restaurants.find(params[:id])
-        restaurant.to_json
+        restaurant.to_json(include: [reservations: {include: { guest: { only: [:name ]}}}])
     end 
 
     post "/restaurants" do 
         restaurant = Restaurant.new(params[:restaurant])
         if restaurant.save
-            restaurant.to_json
+            restaurant.to_json(include: [reservations: {include: { guest: { only: [:name ]}}}])
+
         else
             {errors: restaurant.errors.full_messages}.to_json  
         end 
@@ -22,12 +23,13 @@ class RestaurantsController < ApplicationController
     delete "/restaurants/:id" do 
         restaurant = Restaurant.find(params[:id])
         restaurant.destroy
-        restaurant.to_json
+        restaurant.to_json(include: [reservations: {include: { guest: { only: [:name ]}}}])
+
     end 
 
     patch "/restaurants/:id" do 
         restaurant = Restaurant.find(params[:id])
         restaurant.update(params[:restaurant])
-        restaurant.to_json
+        restaurant.to_json(include: [reservations: {include: { guest: { only: [:name ]}}}])
     end 
 end 
